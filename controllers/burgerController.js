793cc7539813cbc71e0ff22aa33823d540c6c278
Janelle Cueto <janellecueto@gmail.com>
2019-07-30 21:22:25 -0500

    added minor styling

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -19,7 +19,7 @@ router.post("/api/burgers", function(req, res){
     })
 })
 
-router.put("/api/cats/:id", function(req, res){
+router.put("/api/burgers/:id", function(req, res){
     var condition = "id = "+req.params.id;
 
     console.log("condition", condition);
@@ -35,7 +35,7 @@ router.put("/api/cats/:id", function(req, res){
     })
 })
 
-router.delete("api/cats/:id", function(req, res){
+router.delete("api/burgers/:id", function(req, res){
 
 })
 
